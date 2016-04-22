0ddc5678db713fb12aeea2652c7e44538d769dd6
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2016-04-22 15:53:56 +0200

    Update docs

--- implementors/bmemcached/trait.ToMemcached.js
+++ implementors/bmemcached/trait.ToMemcached.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];
+implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];implementors['bmemcached'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
